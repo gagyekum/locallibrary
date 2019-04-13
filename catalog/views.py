7e6672332ccd3279c38da4953b465fb35e434f6f
gagyekum <gide2005@gmail.com>
2019-04-13 12:08:13 +0000

    Added view, template and url mapping for index page and started work on book list page

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, BookInstance, Author
 
 
@@ -18,3 +19,10 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_queryset(self):
+        return Book.objects.all()[:5]
